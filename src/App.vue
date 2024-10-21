1492f9b8a76b6cc15830269c4cda3f86c8730be9
ldvtz <leandro.davatz@gmail.com>
2024-10-21 13:46:28 +0200

    install bcc icons

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,19 @@
 import Topbar from "./components/Topbar.vue"
 
 const post = {
-    link: "https://sssf.bcc.no/korsets-ord-livets-ror-bernt-stadven-1931-2024/",
-    title: "– Korsets ord – Livets ror Bernt Stadven 1931-2024",
-    datePublished: "2024-06-26T12:38:01Z",
-    imageUrl: "https://sssf.bcc.no/wp-content/uploads/2024/06/MBJuliDisplay.jpg",
-    downscaledImageUrl: "https://storage.googleapis.com/widgets-newsfeed-thumbnails-staging/sssf.bcc.nowp-contentuploads202406MBJuliDisplay.jpg.jpg",
-    source: "SSSF"
+  link: "https://sssf.bcc.no/korsets-ord-livets-ror-bernt-stadven-1931-2024/",
+  title: "– Korsets ord – Livets ror Bernt Stadven 1931-2024",
+  datePublished: "2024-06-26T12:38:01Z",
+  imageUrl: "https://sssf.bcc.no/wp-content/uploads/2024/06/MBJuliDisplay.jpg",
+  downscaledImageUrl: "https://storage.googleapis.com/widgets-newsfeed-thumbnails-staging/sssf.bcc.nowp-contentuploads202406MBJuliDisplay.jpg.jpg",
+  source: "SSSF"
 }
 </script>
 
 <template>
   <link href="https://fonts.googleapis.com/css2?family=Archivo:wght@200..900&display=swap" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Serif&display=swap" rel="stylesheet">
-  <Topbar/>
+  <Topbar />
 </template>
 
 <style scoped></style>
