5daad903fc2b68d8d1aeb241479f7d2ca6a7bdf4
ldvtz <leandro.davatz@gmail.com>
2024-10-21 13:10:46 +0200

    Add post data

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <script setup lang="ts">
 import Topbar from "./components/Topbar.vue"
+
+const post = {
+    link: "https://sssf.bcc.no/korsets-ord-livets-ror-bernt-stadven-1931-2024/",
+    title: "– Korsets ord – Livets ror Bernt Stadven 1931-2024",
+    datePublished: "2024-06-26T12:38:01Z",
+    imageUrl: "https://sssf.bcc.no/wp-content/uploads/2024/06/MBJuliDisplay.jpg",
+    downscaledImageUrl: "https://storage.googleapis.com/widgets-newsfeed-thumbnails-staging/sssf.bcc.nowp-contentuploads202406MBJuliDisplay.jpg.jpg",
+    source: "SSSF"
+}
 </script>
 
 <template>
--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -3,6 +3,6 @@
 
 <template>
     <div class="bg-[#DBE5EB] h-16 flex shadow text-center">
-        <h1 class="text-3xl font-bold w-full self-center">Coding is cool</h1>
+        <h1 class="text-3xl font-bold w-full self-center">Coding workshop</h1>
     </div>
 </template>
